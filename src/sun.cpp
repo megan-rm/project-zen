6200bd1ed09b394d1e1f1096ea2522b535cae565
Nathan Miller <nathanmiller77@gmail.com>
2014-08-22 23:52:17 -0500

    trying to fix the sun

--- src/sun.cpp
+++ src/sun.cpp
@@ -26,16 +26,24 @@ void Sun::update()
     *   - Use millisecond time
     * BROKEN. FIX.
     ************************************/
+    /*
     float simple_time = time->get_hour() * 10000;
     simple_time += time->get_minute() * 100;
     simple_time += time->get_second();
-    simple_time /= 235959;
+    simple_time /= 235959;*/
+
+    float simple_time = time->get_hour() * time->get_minute() * time->get_second();
+
+    //simple_time = simple_time * (time->get_minute() * time->get_second());
+
+    simple_time /= 86400;
+
     simple_time *= (2 * M_PI);
 
     float vertical = sin(simple_time);
     float horizontal = (1-cos(simple_time))/2;
 
     move( (0 + (640 - 0) * horizontal), (320 * vertical) + 480);
-    move(320, 240);
+    //move(320, 240);
 };
 
